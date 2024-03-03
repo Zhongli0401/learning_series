479397a7215d9dc194ccf11bdb3f84e683039063
zhongli <76640596+Zhongli0401@users.noreply.github.com>
2024-03-03 12:53:21 +0530

    up

--- java/q.java
+++ java/q.java
@@ -2,8 +2,9 @@ class wjh{
     public static void main(String arg[]){
         double ktr;
         ktr = 3.84;
-        System.out.println("ok");
+        
+        System.out.print("okn ");
         System.out.print(ktr);
-        System.out.println("ok");
+        System.out.println(" ok");
     }
 }
